c1c87ee7966a1546ea122b47d04ed0074816df3c
Sthefane <sthefanesoliveiraa@outlook.com>
2023-08-23 20:25:16 +0100

    created model Product

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 
 app.get('/', (req, res) => {
     res.send('hello api')
 })
 
-app.listen(3000, () => {
-    console.log('node api app running')
+app.get('/blog', (req, res) => {
+    res.send('hello blog sthef')
 })
+
+mongoose.connect("mongodb+srv://sthefanesoliveiraaa:YU1Ti5nW5UcSNrYZ@cluster0.akany63.mongodb.net/Node-API?retryWrites=true&w=majority")
+.then(() => {
+    console.log('connected to mongoDB');
+    app.listen(3000, () => {
+        console.log('node api app running')
+    })
+ })
+.catch(error => {
+    console.log('error', error);
+});
